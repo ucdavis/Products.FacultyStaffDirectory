b0cc0442b0a34276d415cf47a1f32f3bc02b1ea7
Carol McMasters-Stone <cbeck@ucdavis.edu>
2019-09-18 16:01:43 -0700

    repairs to staff view

--- Products/FacultyStaffDirectory/Extensions/newpic.py
+++ Products/FacultyStaffDirectory/Extensions/newpic.py
@@ -11,20 +11,19 @@ from PIL import Image
 def updatePics( self ):
           site = getSite()
           catalog = site.portal_catalog	
-          path = r'/Users/carolm/Desktop/boxphotos/Photos/JPEG'
+          path = r'/Users/carolm/Downloads/croppedpng'
           for dirpath, subdirs, files in os.walk(path):
               for x in (y for y in files if y != ".DS_Store"):
                   xname = x.split("-")
                   personid = xname[2]
                   person = site.people.get(personid)
-                  print person
                   if person:
                       self.plone_log("working on " + person.id)
                       newimage = (os.path.join(path, x))
                       
-                      person.setImage(newimage.read())
-            #               person.image = newimage
-            #                  PIL.Image.open(newimage)
+                      #person.setImage(newimage.read())
+                      #person.image = newimage
+                      PIL.Image.open(newimage)
                       person.image = newimage
                       transaction.commit()
               
