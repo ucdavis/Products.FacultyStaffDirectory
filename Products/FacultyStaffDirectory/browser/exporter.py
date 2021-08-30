7c637ab1dbbd7022b5cda04da70e1177695a4773
Carol McStone <cbeck@ucdavis.edu>
2021-08-30 15:34:17 -0700

    new export

--- Products/FacultyStaffDirectory/browser/exporter.py
+++ Products/FacultyStaffDirectory/browser/exporter.py
@@ -1,14 +1,5 @@
-import csv
-import os
-from io import BytesIO
-from zope.component.hooks import getSite
-from Products.Five.browser import BrowserView
-
-from Products.CMFCore.utils import getToolByName
-
-
 class CSVExport(BrowserView):
-  
+
     def __call__(self):
         buffer = BytesIO()
         encoding = self.request.get('encoding')
@@ -18,62 +9,70 @@ class CSVExport(BrowserView):
             'field_sf_first_name',
             'field_sf_middle_initial',
             'field_sf_last_name',
-            'suffix',
             'field_sf_emails',
             'field_sf_position_title',
             'field_sf_office_location',
-            'officeCity',
-            'officeState',
-            'officePostalCode',
+            'field_sf_office_city',
+            'field_sf_office_state',
+            'field_sf_office_zip',
             'field_sf_phone_numbers',
             'body',
             'field_sf_education_and_degrees',
             'field_sf_websites',
-            'field_sf_honors_and_awards',
-            'image',
+            'field_sf_research_interests',
 
         ]
 
-        writer.writerow(header)	
+        writer.writerow(header)
 
         site = getSite()
         catalog = getToolByName(site, 'portal_catalog')
 
-        
-        targetDeptID = "blue-cluster-staff"
+
+        targetDeptID = "mindbrain"
         department = catalog(portal_type='FSDDepartment', id=targetDeptID)
         #for multiple departments, don't use this next part
         deptobj = department[0].getObject()
         duid = deptobj.UID()
         people = catalog(getRawDepartments=duid, portal_type="FSDPerson")
-        
-        
+        bioHead = """<h3 class="heading--auxiliary">Biography</h3>"""
+        resHead = """<h3 class="heading--auxiliary">Research Focus</h3>"""
+        pubHead = """<h3 class="heading--auxiliary">Research Focus</h3>"""
+        teachHead = """<h3 class="heading--auxiliary">Teaching</h3>"""
+        awdHead = """<h3 class="heading--auxiliary">Awards</h3>"""
+
+
 
         for person in people:
-            
+
                 pobj = person.getObject()
                 memberinfo = deptobj.getMembershipInformation(pobj)
                 #this image url won't work for staff - consider running against cortex
-                imageurl = "https://%s.ucdavis.edu/people/%s/image"%(targetDeptID, pobj.id)
+                #create bio
+                if pobj.research:
+                    bio = ("".join(bioHead,pobj.biography,resHead,pobj.research))
+                if pobj.publications:
+                    bio = ("".join(bio,pubHead,pobj.publications))
+                if pobj.teaching:
+                    bio = ("".join(bio,teachHead,pobj.teaching))
+                if pobj.awards:
+                    bio = ("".join(bio,awdHead,pobj.awards))
                 row = []
                 row.append(pobj.id)
                 row.append(pobj.firstName)
                 row.append(pobj.middleName)
                 row.append(pobj.lastName)
-                row.append(pobj.suffix)
                 row.append(pobj.email)
                 row.append(memberinfo.getPosition())
                 row.append(memberinfo.getDept_officeAddress())
-                row.append(pobj.officeCity)
-                row.append(pobj.officeState)
-                row.append(pobj.officePostalCode)
+                row.append(pobj.getDept_officeCity())
+                row.append(pobj.getDept_officeState())
+                row.append(pobj.getDept_officePostalCode())
                 row.append(memberinfo.getDept_officePhone())
-                row.append(pobj.biography)
+                row.append(bio)
                 row.append(pobj.education)
                 row.append(pobj.websites)
-                row.append(pobj.awards)
-                row.append(imageurl)
-
+                row.append(pobj.getSpecialtyNames())
                 writer.writerow(row)
         value = buffer.getvalue()
 
@@ -85,3 +84,4 @@ class CSVExport(BrowserView):
                         'attachment; filename=export.csv')
 
         return value
+~                                                                                
